ca5c9ffc544e78b99914cb63aa0f1af3dae13ce1
zorbaz77@hotmail.com <zorbaz77@hotmail.com>
2025-08-20 20:37:12 +0000

    More models

---
+++ ninja_dbt/snapshots/snapshot_payments.sql
@@ -0,0 +1,14 @@
+-- snapshots/snapshot_customers.sql
+{% snapshot snapshot_customers %}
+
+{{
+    config(
+        unique_key='customer_id',
+        strategy='timestamp',
+        updated_at='created_at'
+    )
+}}
+
+select * from {{ source('loan_source', 'customers') }}
+
+{% endsnapshot %}
