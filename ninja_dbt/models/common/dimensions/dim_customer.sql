86851c5b22fdddefb4c3f0efb1df0c66eca7bcd8
zorbaz77@hotmail.com <zorbaz77@hotmail.com>
2025-08-20 18:59:31 +0000

    l

--- ninja_dbt/models/common/dimensions/dim_customer.sql
+++ ninja_dbt/models/common/dimensions/dim_customer.sql
@@ -1,27 +1,33 @@
 -- models/dim_customer.sql
+{{ config(materialized="table", unique_key="customer_id") }}
 
-{{
-  config(
-    materialized='table',
-    unique_key='customer_id'
-  )
-}}
+with
+    source as (
+        select
+            {{ dbt_utils.generate_surrogate_key(["customer_id"]) }} as customer_id,
+            cast(customer_id as varchar(255)) as source_customer_number,
+            convert_timezone(
+                to_char(current_timestamp(), 'TZH:TZM'),
+                'GMT',
+                created_at::timestamp_ntz
+            ) as created_datetime_gmt,
+            convert_timezone(
+                to_char(current_timestamp(), 'TZH:TZM'), created_at::timestamp_ntz
+            ) as created_datetime_local,
+            first_name,
+            last_name,
+            email,
+            row_number() over (
+                partition by customer_id order by dbt_valid_from
+            ) as version,
+            case
+                when dbt_valid_to = to_date('9999-12-31') then true else false
+            end as is_current,
+            updated_at,
+            dbt_valid_from,
+            dbt_valid_to
+        from {{ ref("snapshots","snapshot_customers") }}
+    )
 
-with source as (
-    select
-    {{ dbt_utils.generate_surrogate_key(['customer_id']) }}  AS customer_id
-    , CAST(customer_id AS VARCHAR(255)) AS SOURCE_CUSTOMER_NUMBER
-    , CONVERT_TIMEZONE(TO_CHAR(current_timestamp(), 'TZH:TZM'), 'GMT', created_at::TIMESTAMP_NTZ) AS CREATED_DATETIME_GMT
-	, CONVERT_TIMEZONE(TO_CHAR(current_timestamp(), 'TZH:TZM'), created_at::TIMESTAMP_NTZ) AS CREATED_DATETIME_LOCAL
-    , first_name
-    , last_name
-    , email 
-    , row_number() over (partition by customer_id order by dbt_valid_from ) as version
-    , case when dbt_valid_to = to_date('9999-12-31') then true else false end as is_current
-    , updated_at
-    , dbt_valid_from	
-    , dbt_valid_to
-    from {{ ref('snapshots','snapshot_customers') }}
-)
-
-select * from source
+select *
+from source
