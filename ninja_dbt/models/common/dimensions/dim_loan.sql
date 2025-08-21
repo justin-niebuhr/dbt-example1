27c609018770d9468c2976950cf665e20c708701
zorbaz77@hotmail.com <zorbaz77@hotmail.com>
2025-08-21 14:35:09 +0000

    a

--- ninja_dbt/models/common/dimensions/dim_loan.sql
+++ ninja_dbt/models/common/dimensions/dim_loan.sql
@@ -4,34 +4,34 @@
 with
     final as (
         select
-            {{ dbt_utils.generate_surrogate_key(["loan_id"]) }} as loan_id
-            , {{ dbt_utils.generate_surrogate_key(["application_id "]) }} as application_id 
-            , {{ dbt_utils.generate_surrogate_key(["customer_id"]) }} as customer_id
-            , cast(loan_id as varchar(255)) as SOURCE_APPROVED_LOAN_NUMBER
-            , loan_amount as APPROVED_LOAN_AMOUNT
-            , interest_rate as ANNUAL_INTEREST_RATE
-            , start_date as DISBURSEMENT_DATE
-            , end_date as MATURITY_DATE
-            , status as LOAN_STATUS
-            , row_number() over (
+            {{ dbt_utils.generate_surrogate_key(["loan_id"]) }} as loan_id,
+            {{ dbt_utils.generate_surrogate_key(["application_id "]) }}
+            as application_id,
+            {{ dbt_utils.generate_surrogate_key(["customer_id"]) }} as customer_id,
+            cast(loan_id as varchar(255)) as source_approved_loan_number,
+            loan_amount as approved_loan_amount,
+            interest_rate as annual_interest_rate,
+            start_date as disbursement_date,
+            end_date as maturity_date,
+            {{ status_short("loan", "status") }} as application_status,
+            row_number() over (
                 partition by customer_id order by dbt_valid_from
-            ) as version
-            , case
-                when dbt_valid_to is null then true else false
-            end as is_current
-            , dbt_updated_at
-            , dbt_valid_from
-            , case
-                when dbt_valid_to is null then to_date('9999-12-31') 
+            ) as version,
+            case when dbt_valid_to is null then true else false end as is_current,
+            dbt_updated_at,
+            dbt_valid_from,
+            case
+                when dbt_valid_to is null then to_date('9999-12-31')
             end as dbt_valid_to
-        from {{ ref('snapshot_loans') }}
+        from {{ ref("snapshot_loans") }}
     )
 
-{{ dbt_audit(
-    cte_ref="final",
-    created_by="@gitlabs",
-    updated_by="@jniebuhr",
-    created_date="2025-08-21",
-    updated_date="2025-08-21"
-) }}
-
+    {{
+        dbt_audit(
+            cte_ref="final",
+            created_by="@gitlabs",
+            updated_by="@jniebuhr",
+            created_date="2025-08-21",
+            updated_date="2025-08-21",
+        )
+    }}
--- ninja_dbt/models/common/dimensions/dim_loan_application.sql
+++ ninja_dbt/models/common/dimensions/dim_loan_application.sql
@@ -5,37 +5,36 @@ with
     final as (
         select
             {{ dbt_utils.generate_surrogate_key(["application_id"]) }} as application_id
-            , {{ dbt_utils.generate_surrogate_key(["customer_id"]) }} as customer_id
-            , cast(application_id as varchar(255)) as source_application_number
-            , application_date as  APPLICATION_SUBMIT_DATE
-            , loan_amount_requested as REQUESTED_LOAN_AMOUNT
-            , status as APPLICATION_STATUS
-            , convert_timezone(
-                GET_CURRENT_TIMEZONE(),
-                'GMT',
-                updated_at::timestamp_ntz
-            )::timestamp_ntz as updated_at_gmt
-            , convert_timezone(
-                GET_CURRENT_TIMEZONE(), updated_at::timestamp_ntz
-            )::timestamp_ntz as updated_at_local
-            , row_number() over (
+            ,
+            {{ dbt_utils.generate_surrogate_key(["customer_id"]) }} as customer_id,
+            cast(application_id as varchar(255)) as source_application_number,
+            application_date as application_submit_date,
+            loan_amount_requested as requested_loan_amount,
+            {{ status_short("loan_applications", "status") }} as application_status,
+            convert_timezone(
+                get_current_timezone(), 'GMT', updated_at::timestamp_ntz
+            )::timestamp_ntz as updated_at_gmt,
+            convert_timezone(
+                get_current_timezone(), updated_at::timestamp_ntz
+            )::timestamp_ntz as updated_at_local,
+            row_number() over (
                 partition by customer_id order by dbt_valid_from
-            ) as version
-            , case
-                when dbt_valid_to is null then true else false
-            end as is_current
-            , dbt_updated_at
-            , dbt_valid_from
-            , case
-                when dbt_valid_to is null then to_date('9999-12-31') 
+            ) as version,
+            case when dbt_valid_to is null then true else false end as is_current,
+            dbt_updated_at,
+            dbt_valid_from,
+            case
+                when dbt_valid_to is null then to_date('9999-12-31')
             end as dbt_valid_to
-        from {{ ref('snapshot_loan_applications') }}
+        from {{ ref("snapshot_loan_applications") }}
     )
 
-{{ dbt_audit(
-    cte_ref="final",
-    created_by="@gitlabs",
-    updated_by="@jniebuhr",
-    created_date="2025-08-21",
-    updated_date="2025-08-21"
-) }}
+    {{
+        dbt_audit(
+            cte_ref="final",
+            created_by="@gitlabs",
+            updated_by="@jniebuhr",
+            created_date="2025-08-21",
+            updated_date="2025-08-21",
+        )
+    }}
